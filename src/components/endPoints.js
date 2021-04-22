1ff705ed7b0f1161b95282d5fc3a840033b9d452
Neha Chaturvedi <neha.chaturvedi@mozo.com.au>
2021-04-23 00:50:13 +1000

    Add rest api end points

---
+++ src/components/endPoints.js
@@ -0,0 +1,3 @@
+export const getAllRestaurants = 'http://performancetestdmb.azurewebsites.net/api/getallrestaurants'
+export const getProductsPerCategory = 'http://performancetestdmb.azurewebsites.net/api/products'
+export const priceUrl = 'http://performancetestdmb.azurewebsites.net/api/products/123456/Category2'
